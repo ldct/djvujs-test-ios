81176d0b3bd4cebd4746c8f3964f1f19e30a5d3a
Li Xuanji <xuanji@gmail.com>
2023-12-02 13:21:06 -0500

    Initial Commit

---
+++ djvujs-test-ios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  djvujs-test-ios
+//
+//  Created by Li Xuanji on 2/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ djvujs-test-ios/djvujs_test_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  djvujs_test_iosApp.swift
+//  djvujs-test-ios
+//
+//  Created by Li Xuanji on 2/12/23.
+//
+
+import SwiftUI
+
+@main
+struct djvujs_test_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
